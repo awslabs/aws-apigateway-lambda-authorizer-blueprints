e99214b08e541c0aeff9338b19cc8f2ebbd3fd7f
Kohn <jackko@685b3582f871.ant.amazon.com>
2016-02-23 14:19:01 -0800

    Add V1 blueprints. NodeJS and Python are copies of the existing Lambda blueprints, Java is a new example/blueprint

---
+++ blueprints/java/src/example/APIGatewayAuthorizerHandler.java
@@ -0,0 +1,64 @@
+package example;
+
+import io.AuthPolicy;
+import io.TokenAuthorizerContext;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * Handles IO for a Java Lambda function as a custom authorizer for API Gateway
+ * 
+ * @author Jack Kohn
+ *
+ */
+public class APIGatewayAuthorizerHandler implements RequestHandler<TokenAuthorizerContext, AuthPolicy> {
+
+    @Override
+    public AuthPolicy handleRequest(TokenAuthorizerContext input, Context context) {
+    	
+    	String token = input.getAuthorizationToken();
+    	
+    	// validate the incoming token
+        // and produce the principal user identifier associated with the token
+
+        // this could be accomplished in a number of ways:
+        // 1. Call out to OAuth provider
+        // 2. Decode a JWT token in-line
+        // 3. Lookup in a self-managed DB
+        String principalId = "xxxx";
+
+        // if the client token is not recognized or invalid
+        // you can send a 401 Unauthorized response to the client by failing like so:
+        // throw new RuntimeException("Unauthorized");
+
+        // if the token is valid, a policy should be generated which will allow or deny access to the client
+
+        // if access is denied, the client will receive a 403 Access Denied response
+        // if access is allowed, API Gateway will proceed with the back-end integration configured on the method that was called
+        
+    	String methodArn = input.getMethodArn();
+    	String[] arnPartials = methodArn.split(":");
+    	String region = arnPartials[3];
+    	String awsAccountId = arnPartials[4];
+    	String[] apiGatewayArnPartials = arnPartials[5].split("/");
+    	String restApiId = apiGatewayArnPartials[0];
+    	String stage = apiGatewayArnPartials[1];
+    	String httpMethod = apiGatewayArnPartials[2];
+    	String resource = ""; // root resource
+    	if (apiGatewayArnPartials.length == 4) {
+    		resource = apiGatewayArnPartials[3];
+    	}
+    	
+        // this function must generate a policy that is associated with the recognized principal user identifier.
+        // depending on your use case, you might store policies in a DB, or generate them on the fly
+
+        // keep in mind, the policy is cached for 5 minutes by default (TTL is configurable in the authorizer)
+        // and will apply to subsequent calls to any method/resource in the RestApi
+        // made with the same token
+
+        // the example policy below denies access to all resources in the RestApi
+        return new AuthPolicy(principalId, AuthPolicy.PolicyDocument.getDenyAllPolicy(region, awsAccountId, restApiId, stage));
+    }
+
+}
---
+++ blueprints/java/src/io/TokenAuthorizerContext.java
@@ -0,0 +1,54 @@
+package io;
+
+/**
+ * Object representation of input to an implementation of an API Gateway custom authorizer
+ * of type TOKEN
+ * 
+ * @author Jack Kohn
+ */
+public class TokenAuthorizerContext {
+
+    String type;
+    String authorizationToken;
+    String methodArn;
+
+    /**
+     * JSON input is deserialized into this object representation
+     * @param type Static value - TOKEN
+     * @param authorizationToken - Incoming bearer token sent by a client
+     * @param methodArn - The API Gateway method ARN that a client requested
+     */
+    public TokenAuthorizerContext(String type, String authorizationToken, String methodArn) {
+        this.type = type;
+        this.authorizationToken = authorizationToken;
+        this.methodArn = methodArn;
+    }
+
+    public TokenAuthorizerContext() {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAuthorizationToken() {
+        return authorizationToken;
+    }
+
+    public void setAuthorizationToken(String authorizationToken) {
+        this.authorizationToken = authorizationToken;
+    }
+
+    public String getMethodArn() {
+        return methodArn;
+    }
+
+    public void setMethodArn(String methodArn) {
+        this.methodArn = methodArn;
+    }
+}
