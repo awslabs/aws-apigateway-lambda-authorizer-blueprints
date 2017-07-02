1912fceff55fdae1cb30d612690368c9c3905fb8
joedm <caleb@liquidfusion.com.au>
2017-07-02 22:58:22 +1000

    Add blueprint for dot net in c sharp

---
+++ blueprints/dotnet/src/APIGatewayAuthorizerHandler/Model/ApiGatewayArn.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+namespace APIGatewayAuthorizerHandler.Model
+{
+    /// <summary>
+    /// POCO to simplify working with the MethodArn format.
+    /// </summary>
+    public class ApiGatewayArn
+    {
+        public string Arn { get; set; } = "arn";
+        public string Aws { get; set; } = "aws";
+        public string ApiGateway { get; set; } = "execute-api";
+        public string Region { get; set; }
+        public string AwsAccountId { get; set; }
+        public string RestApiId { get; set; }
+        public string Stage { get; set; }
+        public string Method { get; set; }
+        public string Resource { get; set; }
+
+        public override string ToString()
+        {
+            string resourceSuffix = Resource == null ? string.Empty : $"/{Resource}"; // if theres no resource we don't want to end up with an extra "/"
+            return $"{Arn}:{Aws}:{ApiGateway}:{Region}:{AwsAccountId}:{RestApiId}/{Stage}/{Method}{resourceSuffix}";
+        }
+
+        public static ApiGatewayArn Parse(string value)
+        {
+            var result = new ApiGatewayArn();
+            string[] arnSplit = value.Split(':');
+
+            result.Arn = arnSplit[0];
+            result.Aws = arnSplit[1];
+            result.ApiGateway = arnSplit[2];
+
+            result.Region = arnSplit[3];
+            result.AwsAccountId = arnSplit[4];
+            
+            string[] pathSplit = arnSplit[5].Split('/');
+            result.RestApiId = pathSplit[0];
+            result.Stage = pathSplit[1];
+            result.Method = pathSplit[2];
+            
+            if (pathSplit.Length > 3)
+            {
+                result.Resource = string.Join("/", pathSplit.Skip(3));
+            }
+
+            return result;
+        }
+
+        public static bool TryParse(string value, out ApiGatewayArn methodArn)
+        {
+            try
+            {
+                methodArn = Parse(value);
+                return true;
+            }
+            catch
+            {
+                methodArn = null;
+                return false;
+            }
+        }
+    }
+}
---
+++ blueprints/dotnet/src/APIGatewayAuthorizerHandler/Model/Auth/Statement.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace APIGatewayAuthorizerHandler.Model.Auth
+{
+    public class Statement
+    {
+        [JsonProperty(PropertyName = "Action")]
+        public string Action { get; set; }
+        [JsonProperty(PropertyName = "Effect")]
+        public string Effect { get; set; } = "Deny"; // Default to Deny to ensure Allows are explicitly set
+        [JsonProperty(PropertyName = "Resource")]
+        public string Resource { get; set; }
+    }
+}
