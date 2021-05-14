d49eb6a1e2ae4a01688fb2bc861cd463d3ac5eb0
Leonardo <lsantos@buus.com.br>
2021-05-14 11:00:28 -0300

    Fixed misleading comment about stars in resources, and added link to docs

--- blueprints/go/main.go
+++ blueprints/go/main.go
@@ -155,10 +155,21 @@ func NewAuthorizerResponse(principalID string, AccountID string) *AuthorizerResp
 				Version: "2012-10-17",
 			},
 		},
-		Region:    "<<region>>", // Replace the placeholder value with the region where the API is deployed, or '*' for all regions.
+		// Replace the placeholder value with a default region to be used in the policy. 
+		// Beware of using '*' since it will not simply mean any region, because stars will greedily expand over '/' or other separators. 
+		// See https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html for more details. 
+		Region:    "<<region>>", 
 		AccountID: AccountID,
-		APIID:     "<<restApiId>>", // Replace the placeholder value with an API Gateway API id, or '*' for all APIs.
-		Stage:     "<<stage>>", // Replace the placeholder value with the name of the stage used in the policy, or '*' for all stages.
+		
+		// Replace the placeholder value with a default API Gateway API id to be used in the policy. 
+		// Beware of using '*' since it will not simply mean any API Gateway API id, because stars will greedily expand over '/' or other separators. 
+		// See https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html for more details. 
+		APIID:     "<<restApiId>>", 
+		
+		// Replace the placeholder value with a default stage to be used in the policy. 
+		// Beware of using '*' since it will not simply mean any stage, because stars will greedily expand over '/' or other separators. 
+		// See https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_resource.html for more details. 
+		Stage:     "<<stage>>", 
 	}
 }
 
