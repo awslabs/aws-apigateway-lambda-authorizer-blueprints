f19db0fb646905fa9392fe33ca12197c84a7ba60
Melrick Mascarenhas <melrickjm@gmail.com>
2022-04-03 14:55:33 -0700

    ARN fix for China regions. Issue #25

--- blueprints/python/api-gateway-authorizer-python.py
+++ blueprints/python/api-gateway-authorizer-python.py
@@ -133,7 +133,16 @@ class AuthPolicy(object):
         if resource[:1] == "/":
             resource = resource[1:]
 
-        resourceArn = ("arn:aws:execute-api:" +
+        if re.search("cn", self.region):
+            resourceArn = ("arn:aws-cn:execute-api:" +
+            self.region + ":" +
+            self.awsAccountId + ":" +
+            self.restApiId + "/" +
+            self.stage + "/" +
+            verb + "/" +
+            resource)
+        else:
+            resourceArn = ("arn:aws:execute-api:" +
             self.region + ":" +
             self.awsAccountId + ":" +
             self.restApiId + "/" +
